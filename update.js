ccb2844be4d5b6be584a8be84cbbc4b7410b0440
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-30 02:45:17 +0200

    fix update scripts

--- update.js
+++ update.js
@@ -8,7 +8,7 @@ const author = authors[0];
 getTweets(tokens, 'cssunderhood', author.first, (err, tweets, missed, info) => {
   if (err) throw err;
   author.tweets = tweets;
-  const done = err => console.log(`${author.username} done`;
+  const done = err => console.log(`${author.username} done`);
   fs.outputJson(`./dump/${author.username}.json`, author, done);
   fs.outputJson(`./dump/${author.username}-info.json`, info, done);
 });
