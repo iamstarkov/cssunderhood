9872177ec11df14450464ceff0d111d1c836196b
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-10 00:42:52 +0200

    publish @vdv73rus

--- authors.js
+++ authors.js
@@ -1,5 +1,5 @@
 export default [
   // post: false
-  { username: 'vdv73rus',     start: '03 Aug 2015', first: '628084678298349569', post: false },
+  { username: 'vdv73rus',     start: '03 Aug 2015', first: '628084678298349569' },
   { username: 'denisfedosov', start: '24 Jul 2015', first: '624454034267742208' },
 ];
