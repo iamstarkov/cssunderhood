523a1b98cf2cfc657398f453cec6f4c9696d3880
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-24 00:18:20 +0200

    publish @delaz

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520', post: false },
+  { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520' },
   { username: 'denysdovhan',  start: '10 Aug 2015', first: '630688946222141440' },
   { username: 'vdv73rus',     start: '03 Aug 2015', first: '628084678298349569' },
   { username: 'denisfedosov', start: '24 Jul 2015', first: '624454034267742208' },
