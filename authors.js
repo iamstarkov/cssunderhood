73e6b448f012e2abb296d799e05e4e3aee17b029
Zyuzin Vitaly <juwain@yandex.ru>
2015-08-24 10:30:45 +0300

    add @SelenIT2

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // post: false
+  { username: 'SelenIT2',     start: '24 Aug 2015', first: '635698636509323264', post: false },
   { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520' },
   { username: 'denysdovhan',  start: '10 Aug 2015', first: '630688946222141440' },
   { username: 'vdv73rus',     start: '03 Aug 2015', first: '628084678298349569' },
