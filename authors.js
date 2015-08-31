d3ad3300e1e6b7c958943b44061c2013a72a320e
Zyuzin Vitaly <juwain@yandex.ru>
2015-08-31 10:46:45 +0300

    add @meuwka

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // post: false
+  { username: 'meuwka',       start: '31 Aug 2015', first: '638242826489307136', post: false },
   { username: 'SelenIT2',     start: '24 Aug 2015', first: '635698636509323264' },
   { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520' },
   { username: 'denysdovhan',  start: '10 Aug 2015', first: '630688946222141440' },
