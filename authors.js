0fa8c7e3d660264ded3895ac7dee878b265d8241
Zyuzin Vitaly <juwain@yandex.ru>
2015-09-14 00:02:39 +0300

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'harisov',      start: '07 Sep 2015', first: '640739893522640896', post: false },
+  { username: 'harisov',      start: '07 Sep 2015', first: '640739893522640896' },
   { username: 'meuwka',       start: '31 Aug 2015', first: '638242826489307136' },
   { username: 'SelenIT2',     start: '24 Aug 2015', first: '635698636509323264' },
   { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520' },
