5196bcb0ce3228bd8c3b11a419294dca472fb00a
Zyuzin Vitaly <juwain@yandex.ru>
2015-09-21 08:55:52 +0300

    publish @mr_skriming

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'mr_skriming',  start: '14 Sep 2015', first: '643312253723406337', post: false },
+  { username: 'mr_skriming',  start: '14 Sep 2015', first: '643312253723406337' },
   { username: 'harisov',      start: '07 Sep 2015', first: '640739893522640896' },
   { username: 'meuwka',       start: '31 Aug 2015', first: '638242826489307136' },
   { username: 'SelenIT2',     start: '24 Aug 2015', first: '635698636509323264' },
