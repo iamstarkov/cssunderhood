a9e591e52ac8a08e9ddea8c9c92b0819b90bbde2
Zyuzin Vitaly <juwain@yandex.ru>
2015-09-28 11:24:46 +0300

    add @arseniydmitriev

--- authors.js
+++ authors.js
@@ -1,12 +1,13 @@
 export default [
   // post: false
-  { username: 'Akurganow',    start: '22 Sep 2015', first: '645859313605545984' },
-  { username: 'mr_skriming',  start: '14 Sep 2015', first: '643312253723406337' },
-  { username: 'harisov',      start: '07 Sep 2015', first: '640739893522640896' },
-  { username: 'meuwka',       start: '31 Aug 2015', first: '638242826489307136' },
-  { username: 'SelenIT2',     start: '24 Aug 2015', first: '635698636509323264' },
-  { username: 'delaz',        start: '17 Aug 2015', first: '633223592566763520' },
-  { username: 'denysdovhan',  start: '10 Aug 2015', first: '630688946222141440' },
-  { username: 'vdv73rus',     start: '03 Aug 2015', first: '628084678298349569' },
-  { username: 'denisfedosov', start: '24 Jul 2015', first: '624454034267742208' },
+  { username: 'arseniydmitriev', start: '28 Sep 2015', first: '648383508948365312', post: false },
+  { username: 'Akurganow',       start: '22 Sep 2015', first: '645859313605545984' },
+  { username: 'mr_skriming',     start: '14 Sep 2015', first: '643312253723406337' },
+  { username: 'harisov',         start: '07 Sep 2015', first: '640739893522640896' },
+  { username: 'meuwka',          start: '31 Aug 2015', first: '638242826489307136' },
+  { username: 'SelenIT2',        start: '24 Aug 2015', first: '635698636509323264' },
+  { username: 'delaz',           start: '17 Aug 2015', first: '633223592566763520' },
+  { username: 'denysdovhan',     start: '10 Aug 2015', first: '630688946222141440' },
+  { username: 'vdv73rus',        start: '03 Aug 2015', first: '628084678298349569' },
+  { username: 'denisfedosov',    start: '24 Jul 2015', first: '624454034267742208' },
 ];
