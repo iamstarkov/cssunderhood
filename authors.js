c465afc9287bf006155e2f1afc371c78f3b230f6
Zyuzin Vitaly <juwain@yandex.ru>
2015-11-15 23:30:48 +0300

    publish @talgautb

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false 
-  { username: 'talgautb',        start: '09 Nov 2015', first: '663553316618502144', post: false },
+  { username: 'talgautb',        start: '09 Nov 2015', first: '663553316618502144' },
   { username: 'vafelman',        start: '02 Nov 2015', first: '661129183708512257' },
   { username: 'exdeniz',         start: '26 Oct 2015', first: '658538212172898304' },
   { username: 'mishcherin',      start: '19 Oct 2015', first: '656004895087206400' },
